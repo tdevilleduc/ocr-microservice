931105fd4738ef086591bca95eaecc3d83b5befc
Unknown <devilleduc.thomas@gmail.com>
2019-04-26 17:16:57 +0200

    [feat] utilisation de la base de données h2 pour alimenter le productDao
    suppression ProductDaoImpl pour utilisation la generation de code de JPA

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -1,12 +1,11 @@
 package com.ecommerce.microcommerce.dao;
 
 import com.ecommerce.microcommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
+@Repository
+public interface ProductDao extends JpaRepository<Product, Integer> {
 
-public interface ProductDao {
-
-    public List<Product> findAll();
-    public Product findById(int id);
-    public Product save(Product product);
+    Product findById(int id);
 }
--- src/main/java/com/ecommerce/microcommerce/model/Product.java
+++ src/main/java/com/ecommerce/microcommerce/model/Product.java
@@ -3,9 +3,15 @@ package com.ecommerce.microcommerce.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(value = {"id", "prixAchat"})
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue
     private int id;
     private String nom;
     private int prix;
