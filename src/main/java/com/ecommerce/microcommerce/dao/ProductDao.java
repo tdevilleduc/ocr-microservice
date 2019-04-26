30e7b4f1c75a97a9eb47649586cd842c3a83a71f
Unknown <devilleduc.thomas@gmail.com>
2019-04-26 15:43:21 +0200

    [feat] ajout controller, repository et model pour product
    deux routes : get Produits/{id} (findOne) et get Produits (findAll)

--- src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
+++ src/main/java/com/ecommerce/microcommerce/dao/ProductDao.java
@@ -1,4 +1,12 @@
 package com.ecommerce.microcommerce.dao;
 
-public class ProductDao {
+import com.ecommerce.microcommerce.model.Product;
+
+import java.util.List;
+
+public interface ProductDao {
+
+    public List<Product> findAll();
+    public Product findById(int id);
+    public Product save(Product product);
 }
--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,4 +1,28 @@
 package com.ecommerce.microcommerce.web.controller;
 
+import com.ecommerce.microcommerce.dao.ProductDao;
+import com.ecommerce.microcommerce.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
 public class ProductController {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @GetMapping(value = "/Produits/{id}")
+    public Product afficherUnProduit(@PathVariable int id) {
+        return productDao.findById(id);
+    }
+
+    @GetMapping(value = "/Produits")
+    public List<Product> listeProduits() {
+        return productDao.findAll();
+    }
 }
